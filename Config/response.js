7b86493394cf39c0667c3078ee19cf65c99a89b4
zahiracantika <zahira.19411005@student.ubl.ac.id>
2022-01-03 17:21:35 +0700

    menyimpan dan menampilkan data flutter

--- Config/response.js
+++ Config/response.js
@@ -1,35 +1,35 @@
 module.exports = {
     errorResponse: (msg) => {
-        return {
-            status: false,
-            message: msg
-        }
+      return {
+        status: false,
+        message: msg
+      }
     },
     suksesResponse: (msg) => {
-        return {
-            status: true,
-            message: msg
-        }
+      return {
+        status: true,
+        message: msg
+      }
     },
     suksesResult: (data) => {
-        return {
-            sukses: true,
-            msg: 'Berhasil Mendapat Data',
-            result: data
-        }
+      return {
+        status: true,
+        message: 'Berhasil Mendapatkan Data',
+        result: data
+      }
     },
     nullResult: () => {
-        return {
-            sukses: false,
-            msg: 'Tidak Ada Data',
-            result: null
-        }
+      return {
+        sukses: false,
+        msg: 'Tidak Ada Data',
+        result: null
+      }
     },
     errorResult: () => {
-        return {
-            sukses: false,
-            msg: 'Gagal Mendapatkan Data',
-            data: null
-        }
+      return {
+        sukses: false,
+        msg: 'Gagal Mendapatkan Data',
+        data: []
+      }
     }
-}
+  }
---
+++ Controller/sepatu_controller.js
@@ -0,0 +1,31 @@
+/*
+Lampung Selatan
+Indonesia
+*/
+const Model= require("../Model/model_sepatu");
+const response = require("../Config/response");
+
+exports.get = (data) =>
+  new Promise((resolve, reject) => {
+    Model.find().then((data) => {
+      if (data.length == 0) {
+        reject(response.errorResult());
+      } else {
+        resolve(
+          Object.assign(response.suksesResult("Succes"), {
+            data: data,
+          })
+        );
+      }
+    });
+  });
+
+exports.add = (data) =>
+  new Promise((resolve, reject) => {
+    Model.create(data)
+      .then(() =>
+        resolve(response.suksesResponse("Berhasil Menambah Data Sepatu"))
+      )
+      .catch(() => reject(response.errorResponse("Gagal Menambah Data Sepatu")));
+  });
+  
